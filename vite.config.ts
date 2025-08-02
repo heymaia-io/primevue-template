4a077d82db07a8900f0d064d9d9f67ec59c3f485
Israel Santiago <github@ultron.email>
2025-08-02 02:42:52 -0700

    feat: update TypeScript and Vite configurations, add new dependencies, and improve module resolution

--- vite.config.ts
+++ vite.config.ts
@@ -3,21 +3,29 @@ import { PrimeVueResolver } from '@primevue/auto-import-resolver';
 import vue from '@vitejs/plugin-vue';
 import Components from 'unplugin-vue-components/vite';
 import { defineConfig } from 'vite';
+import VueRouter from 'unplugin-vue-router/vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    optimizeDeps: {
-        noDiscovery: true
+  define: {
+    // enable hydration mismatch details in production build
+    __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: 'true',
+  },
+  optimizeDeps: {
+    noDiscovery: true,
+  },
+  plugins: [
+    VueRouter({
+      /* options */
+    }),
+    vue(),
+    Components({
+      resolvers: [PrimeVueResolver()],
+    }),
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
-    plugins: [
-        vue(),
-        Components({
-            resolvers: [PrimeVueResolver()]
-        })
-    ],
-    resolve: {
-        alias: {
-            '@': fileURLToPath(new URL('./src', import.meta.url))
-        }
-    }
+  },
 });
